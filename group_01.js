81a237ebe6c3162ddfca28bae192854f3906e947
jamesleary96@yahoo.com <administrator@Administrators-MacBook-Pro.local>
2017-06-19 16:34:43 -0500

    completed groupwork.prime_group_js_01

--- group_01.js
+++ group_01.js
@@ -6,3 +6,51 @@ var robert = ["Robert", "26835", "66000", 1];
 var mayella = ["Mayella", "89068", "35000", 2];
 
 var employees = [atticus, jem, boo, scout, robert, mayella];
+
+
+var bonus = function (employee) {
+  for (var i = 0; i < employees.length; i++) {
+    var bonusCheck = [];
+    bonusCheck[0] = employee[i][0];
+
+    if (employee[i][3] < 3) {
+      bonusCheck[1]= 0;
+    }
+    else if (employee[i][3] === 3) {
+      bonusCheck[1]= 4;
+    }
+    else if (employee[i][3] === 4) {
+      bonusCheck[1]= 6;
+    }
+    else if (employee[i][3] === 5) {
+      bonusCheck[1]= 10;
+    }
+
+   if (employee[i][1].length === 4) {
+      bonusCheck[1] += 5;
+    }
+
+   if(employee[i][2] > 65000){
+        bonusCheck[1] -=1;
+    }
+
+   if(bonusCheck[1]>13){
+      bonusCheck[1]=13;
+    } else if (bonusCheck[1]<0) {
+    bonusCheck[1] = 0;
+    }
+
+   bonusCheck [3] = Math.round(employee[i][2]*(bonusCheck[1]/100));
+
+   bonusCheck[2] = bonusCheck[3] + Number.parseInt(employee[i][2]);
+
+ // var node = document.createElement("LI");
+ // var textnode = document.createTextNode(bonusCheck[0]);
+ // node.appendChild(textnode);
+ // document.getElementById("employeeList").apppendChild(node);
+
+  console.log(bonusCheck);
+ }
+};
+
+bonus(employees);
